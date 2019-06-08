ac00a2309bf3323b022c67f435cba25b51600acc
afozbek <furkanozbek1995@gmail.com>
2019-06-08 23:08:57 +0300

    updating reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,14 +1,67 @@
-import * as actionTypes from "./action-types";
+import {
+  INCREMENT_LENGTH,
+  DECREMENT_LENGTH,
+  RESET_CONTENT,
+  CALCULATE_REMAINING
+} from "./action-types";
 
 const initialState = {
-  isResetting: null
+  breakLength: 5,
+  sessionLength: 25,
+  remMinutes: 25,
+  remSeconds: 0,
+  timerStarted: false
 };
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+    case INCREMENT_LENGTH:
+      return incrementLength(state, action);
+    case DECREMENT_LENGTH:
+      return decrementLength(state, action);
+    case RESET_CONTENT:
+      return resetContent(state, action);
+    case CALCULATE_REMAINING:
+      return calculateRemaining(state, action);
     default:
       return state;
   }
+};
+
+const incrementLength = (state, action) => {
+  if (action.payload.isBreak) {
+    return {
+      ...state,
+      breakLength: state.breakLength + action.payload.value
+    };
+  }
+  return {
+    ...state,
+    sessionLength: state.sessionLength + action.payload.value
+  };
+};
+
+const decrementLength = (state, action) => {
+  if (action.payload.isBreak) {
+    return {
+      ...state,
+      breakLength: state.breakLength - action.payload.value
+    };
+  }
+  return {
+    ...state,
+    sessionLength: state.sessionLength - action.payload.value
+  };
+};
+
+const resetContent = (state, action) => {
+  return {
+    ...initialState
+  };
+};
+
+const calculateRemaining = (state, action) => {
+  // TODO
 };
 
 export default rootReducer;
