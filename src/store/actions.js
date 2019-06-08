a88012f332019d2c18fd6b62a0a7492af105631a
afozbek <furkanozbek1995@gmail.com>
2019-06-08 23:08:47 +0300

    adding new actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,7 +1,35 @@
-import * as actionTypes from "./action-types";
+import {
+  INCREMENT_LENGTH,
+  DECREMENT_LENGTH,
+  RESET_CONTENT,
+  CALCULATE_REMAINING
+} from "./action-types";
+
+export const incrementLength = payload => {
+  return {
+    type: INCREMENT_LENGTH,
+    payload
+  };
+};
+
+export const decrementLength = payload => {
+  return {
+    type: DECREMENT_LENGTH,
+    payload
+  };
+};
 
 export const resetContent = () => {
   return {
-    type: actionTypes.RESET_CONTENT
+    type: RESET_CONTENT
   };
 };
+
+export const calculateRemaining = payload => {
+  return {
+    type: CALCULATE_REMAINING,
+    payload
+  };
+};
+
+// ! Start or Stop Timer function
