a14ef31d4a26ceeacc2ce6367646c173083d6b0f
afozbek <furkanozbek1995@gmail.com>
2019-06-09 13:27:36 +0300

    updating Timer

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -2,18 +2,39 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { calculateRemaining } from "../../store/actions";
+import useInterval from "../../hooks/useInterval";
 
 const Timer = props => {
-  let newDate = new Date(null);
-  newDate.setMinutes(props.remMinutes, props.remSeconds, 0);
-  let newDateStr = newDate.toISOString().substr(14, 5);
-  console.log(newDateStr);
-
+  // newDate.setMinutes(props.remMinutes, props.remSeconds, 0);
+  // newDate.toISOString();
+  // ! My Time Interval
+  useInterval(
+    () => {
+      props.calculateRemaining({
+        remMinutes: props.remMinutes,
+        remSeconds: props.remSeconds,
+        label: props.label
+      });
+    },
+    props.timerStarted ? 1000 : null
+  );
+  let style = null;
+  if (props.remMinutes === 0 && props.remSeconds < 60) {
+    style = { color: "rgb(165,13,13)" };
+  }
   return (
     <div className="timer">
-      <div className="timer-wrapper">
+      <div className="timer-wrapper" style={style}>
         <div id="timer-label">{props.label}</div>
-        <div id="time-left">{props.timeLeft}</div>
+        <div id="time-left">
+          <span>
+            {props.remMinutes < 10 ? "0" + props.remMinutes : props.remMinutes}
+          </span>
+          <span> : </span>
+          <span>
+            {props.remSeconds < 10 ? "0" + props.remSeconds : props.remSeconds}
+          </span>
+        </div>
       </div>
     </div>
   );
@@ -22,7 +43,8 @@ const Timer = props => {
 const mapStateToProps = state => ({
   label: state.label,
   remMinutes: state.remMinutes,
-  remSeconds: state.remSeconds
+  remSeconds: state.remSeconds,
+  timerStarted: state.timerStarted
 });
 
 // * I must dispatch this event
