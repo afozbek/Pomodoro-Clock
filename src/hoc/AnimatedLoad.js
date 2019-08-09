500c6f179f42f7ebfd03a5cbcdcb378dcc1c61d0
afozbek <furkanozbek1995@gmail.com>
2019-08-09 21:14:31 +0300

    adding spaces

--- src/hoc/AnimatedLoad.js
+++ src/hoc/AnimatedLoad.js
@@ -3,11 +3,13 @@ import React from "react";
 const AnimateLoad = WrappedComponent => {
   return class extends React.Component {
     state = { didMount: false };
+
     componentDidMount() {
       setTimeout(() => {
         this.setState({ didMount: true });
       }, 0);
     }
+
     render() {
       const { didMount } = this.state;
       return (
